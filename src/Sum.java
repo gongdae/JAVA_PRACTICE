aa8858e13a9b25a18e689d05e77cca0d5e4276da
NA <NA@DESKTOP-A7PGM1M>
2018-02-07 18:59:58 +0900

    두수의 합 구하기

---
+++ src/Sum.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Sum {
+	public static void main(String[] args) {
+		int a,b;
+		System.out.println("입력할 수 : ");
+		Scanner scanner=new Scanner(System.in);
+		String s1,s2;
+		s1=scanner.next();
+		s2=scanner.next();
+		
+		
+		
+		a=Integer.parseInt(s1);
+		b=Integer.parseInt(s2);
+		
+		System.out.println(a+" "+b);
+	}
+
+}
