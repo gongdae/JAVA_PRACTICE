e154c9f0b652e8a492c6320ba3537f29990edd6a
NA <NA@DESKTOP-A7PGM1M>
2018-02-07 12:16:46 +0900

    구구단(메소드)

--- src/gugudan.java
+++ src/gugudan.java
@@ -1,16 +1,31 @@
-import java.util.Scanner;
-
 public class gugudan {
-	public static void main(String[] args)
+	public static int[] caculate(int times)
 	{
 		int[] result=new int[9];
+		
 		for(int i=0;i<result.length;i++)
 		{
-			result[i]=(i+1)*2;
+			result[i]=(i+1)*times;
 		}
+		return result;
+	}
+	
+	public static void print(int[] result)
+	{
 		for(int i=0;i<result.length;i++)
 		{
 			System.out.println(result[i]);
+		}
+	}
+	
+	public static void main(String[] args)
+	{
+		int[] gugudan=new int[9];
+		
+		for(int i=2;i<10;i++)
+		{
+			gugudan=caculate(i);
+			print(gugudan);
 		}
 	}
 } 
