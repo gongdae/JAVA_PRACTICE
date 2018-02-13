ee2acd819ac91bb3564dcf847dbb18182dc91d57
NA <NA@DESKTOP-A7PGM1M>
2018-02-13 17:36:07 +0900

    캔린더(반복추가)

--- src/Calendar.java
+++ src/Calendar.java
@@ -17,13 +17,21 @@ public class Calendar {
 		System.out.println("22 23 24 25 26 27 28");
 		
 		//숫자를 받아서 해당 달의 최대일수 출력
-		System.out.println("원하는 달을 입력하세요 : ");
 		Scanner scanner=new Scanner(System.in);
 		Calendar cal=new Calendar();
+		
+		System.out.println("반복횟수를 입력하세요 : ");
+		int repeat=scanner.nextInt();
+		
+		for(int i=0;i<repeat;i++)
+		{
+		System.out.println("원하는 달을 입력하세요 : ");
 		int month=scanner.nextInt();
+		System.out.println(month+"달은"+cal.getMaxDaysOfMonth(month)+"까지 입니다");
+		}
+		
 		scanner.close();
 		
-		System.out.println(cal.getMaxDaysOfMonth(month)+"입니다");
 		
 	}
 }
